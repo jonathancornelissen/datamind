21eda48f44b6b675fe2955e145c622c8ad268ee1
Jonathan Cornelissen <Jonathancornelissen@hotmail.com>
2013-08-17 11:22:22 +0200

    foralbert temp version

--- R/sandbox.R
+++ R/sandbox.R
@@ -1,12 +1,22 @@
 # Goal: have as little functions as possible a teacher has to use to create and upload a chapter to datamind 
-# Extra functionality will be added in a later phase, feel free to contribute or email suggestions at jonathan@datamind.org! 
+# Extra functionality will be added in a later phase. 
+# Feel free to contribute or just email suggestions at jonathan@datamind.org! 
 
 # 1. Author chapter:
 author_chapter = function(chapdir, ...){
-  author(deckdir=chapdir,  scaffold = system.file('skeleton', package = 'datamind'), ...)  
+  author(deckdir=chapdir,  scaffold = system.file('skeleton', package = 'datamind'), ...) 
 }
 
 # 2. Preview chapter:
+preview_chapter = function(inputFile,outputFile,...){
+  payload = suppressWarnings(slidify(inputFile,return_page=TRUE,...) );
+  
+  # Show the preview:
+  if (missing(outputFile)){
+    outputFile = gsub("*.[R]?md$", '.html', inputFile)
+  }
+  browseURL(outputFile)
+}
 
 # 3. Log in to DataMind:
 datamind_login = function(email, password){
@@ -25,30 +35,34 @@ datamind_login = function(email, password){
 
 # 4. Upload chapter:
 upload_chapter = function( inputFile, ... ){ 
-  payload = slidify(inputFile, return_page=TRUE,...);  # Get the payload  
+  # not efficient, needs refactoring
+  payload = suppressWarnings(slidify(inputFile, return_page=TRUE,...));  # Get the payload  
   theJSON = render_chapter_json_for_datamind(payload); # Get the JSON
   upload_json(theJSON); # Upload everything
 }
 
 ##### HELP FUNCTIONS ##### 
 upload_json = function(theJSON){ 
+  if(!exists(".DATAMIND_ENV")){
+    stop("Please login to DataMind first, using the datamind_login function");    
+  }
   base_url = "http://staging.api.datamind.org/chapters/create_from_r.json";
   redirect_url = "http://test.datamind.org/#/edit_course/";
-  auth_token = .DATAMIND_ENV$auth_token;
-  if(is.null(auth_token)){ 
-    stop("Please login to DataMind first, using the datamind_login function")
-  }else{
+  auth_token = .DATAMIND_ENV$auth_token;    
   url = paste0(base_url,"?auth_token=", auth_token);
+    
   x = POST(url=url, body = theJSON, add_headers("Content-Type" = "application/json"))   
   course_id = content(x)$id;
   message("Course succesfully uploaded to DataMind.org");
   redirect_url = paste0(redirect_url,course_id);
   browseURL(redirect_url)
-  }
 } 
 
-
 render_chapter_json_for_datamind = function(payload){
+  if(!exists(".DATAMIND_ENV")){
+    stop("Please login to DataMind first, using the datamind_login function");    
+  }
+
   # Extract basic course and chapter info: 
   outputList = list(course = payload$courseTitle, 
                     email  = .DATAMIND_ENV$email,
@@ -60,32 +74,36 @@ render_chapter_json_for_datamind = function(payload){
   ) 
   
   # Extract for each exercise the relevant information:
-  slides = payload$slides;
-  exerciseList = list();
+  slides = payload$slides; 
+  exerciseList = list(); 
   for(i in 1:length(slides)){
     slide = slides[[i]]
-    exerciseList[[i]] = list(  title    = slide$title,
+    exerciseList[[i]] = list(  title         = html2txt(slide$title),
                                assignment    = slide$content, 
                                number        = slide$num,
                                instructions  = slide$instructions$content, 
                                hint          = slide$hint$content,
                                sample_code   = extract_code( slide$sample_code$content ),
                                solution      = extract_code( slide$solution$content ),
-                               sct           = extract_code( slide$sct$content) )
+                               sct           = extract_code( slide$sct$content), 
+                               pre_exercise_code = extract_code( slide$pre_exercise_code$content) )
   }
   
   # Join everything: 
-  outputList$chapter$exercises = exerciseList;  
+  outputList$chapter$exercises = exerciseList; 
   
-  # Make JSON:
-  toJSON(outputList)
+  # Make JSON: 
+  toJSON(outputList) 
 }
 
 extract_code = function(html){
+  if(!(is.null(html)|nchar(x)==0)){
   r = regexpr("<code class=\"r\">(.*?)</code>",html);
   code = regmatches(html,r);
   code = gsub("<code class=\"r\">|</code>","",code)
+  code = html2txt(code)
   return(code)
+  }
 }
 
 
@@ -116,4 +134,8 @@ check_exercise_sct = function(exercise){
   return(exercise)  
 }
 
-
+# Convenience function to convert html codes
+html2txt <- function(str) {
+  str = paste0("<code>",str,"</code>");
+  xpathApply(htmlParse(str, asText=TRUE),"//body//text()", xmlValue)[[1]];
+}
--- README.md
+++ README.md
@@ -3,4 +3,4 @@ datamind helps you to create interactive courses with R for the [DataMind platfo
 This package is a wrapper around the [slidify package](http://www.slidify.org) of  Ramnath Vaidyanathan. Over time We'll keep adding functionality to datamind which is specific to the creation of interactive R courses for [DataMind](http://www.datamind.org). Don't hesitate to contribute ;-). 
 
 # Get started with datamind 
-Read the blogpost to get started! (todo :-)) or [Teach on DataMind](http://www.datamind.org/#/dashboard).
+Read the blogpost or the tutorial to get started! (todo :-))!! 
