c79caed263c28f62520399b521d015191d938afc
Jonathan Cornelissen <Jonathancornelissen@hotmail.com>
2013-09-10 15:29:13 +0200

    splitting SCT and course creation functionality, for server-side reasons

--- R/check_existence.R
+++
@@ -1,9 +0,0 @@
-check_existence <- function( variables = NULL ){
-  if ( !is.null(variables) ){ 
-    it.exists <-  sapply(variables,exists)
-    if ( any(!it.exists) ){
-      to.print <- paste("It looks like you didn't define the variable:", variables[!it.exists] )
-      return( list(FALSE,to.print) )
-    } 
-  } 
-}
